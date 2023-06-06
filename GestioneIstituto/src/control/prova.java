d12a4f8a0eb0f96c5fe72e1134aa37bebfa1718d
Vincenzo Luigi Bruno <73852936+vlb20@users.noreply.github.com>
2023-06-06 19:32:34 +0200

    Create prova.java

---
+++ GestioneIstituto/src/control/prova.java
@@ -0,0 +1,15 @@
+package control;
+
+import java.sql.SQLException;
+
+import entity.EntityStudente;
+
+public class prova {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		EntityStudente s=new EntityStudente("1");
+		System.out.println(s);
+	}
+
+}
