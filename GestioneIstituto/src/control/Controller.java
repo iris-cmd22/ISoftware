a6747c8ba011285b7235205c7881acb49ccb2bb8
Cris <111997390+iris-cmd22@users.noreply.github.com>
2023-06-07 09:38:05 +0200

    Scheletro Controller

---
+++ GestioneIstituto/src/control/Controller.java
@@ -0,0 +1,40 @@
+package control;
+
+import java.util.ArrayList;
+
+public class Controller {
+
+    public Controller(){
+
+    }
+
+    public static int registraUtente(){
+
+        int ret=0;
+
+        return ret;
+    }
+
+    public static int inserisciClasse(int idClasse, String sezione, String anno, int annoscolastico, ArrayList<Int> idmaterie){
+
+        int ret=0;
+
+        return ret;
+    }
+
+    public static int aggiungiVoto(){
+
+        int ret=0;
+
+        return ret;
+    }
+    
+    public static ArrayList<String> visualizzaVoti(String usernameGenitore){
+
+        ArrayList<String> valutazioni = new ArrayList<String>();
+
+        return valutazioni;
+    }
+
+
+}
