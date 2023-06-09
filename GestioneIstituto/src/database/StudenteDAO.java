db344b8636bd82db440e0def9c6ebd0664d73ccc
afladrosa <128139011+afladrosa@users.noreply.github.com>
2023-06-10 00:32:27 +0200

    Update StudenteDAO.java

--- GestioneIstituto/src/database/StudenteDAO.java
+++ GestioneIstituto/src/database/StudenteDAO.java
@@ -79,7 +79,7 @@ public class StudenteDAO {
 					//NB:
 					ValutazioneDAO valutazione =new ValutazioneDAO();
 					valutazione.setIdvalutazioni(rs.getInt("idvalutazioni"));
-					valutazione.setData(rs.getString("data"));
+					valutazione.setData(rs.getDate("data"));
 					valutazione.setVoto(rs.getFloat("voto"));
 					
 					this.valutazioni.add(valutazione);
