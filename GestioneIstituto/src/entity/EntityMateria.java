ddf0bce116e69fe495ef5c5c5c45c2482b689c3e
afladrosa <128139011+afladrosa@users.noreply.github.com>
2023-06-09 10:29:41 +0200

    Update EntityMateria.java

--- GestioneIstituto/src/entity/EntityMateria.java
+++ GestioneIstituto/src/entity/EntityMateria.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.util.ArrayList;
 
+import database.MateriaDAO;
 import database.ValutazioneDAO;
 
 public class EntityMateria {
@@ -10,17 +11,45 @@ public class EntityMateria {
     private String nome;
     private ArrayList<EntityValutazione> valutazioni;
 
-    public EntityMateria(int idmaterie) {
-		this.idmaterie = idmaterie;
-		
-	}
-
 	public EntityMateria() {
 		// TODO Auto-generated constructor stub
 		super();
         this.valutazioni =new ArrayList<EntityValutazione>();
 	}
+	
+	//costruttore con la PK
+	public EntityMateria(int idmaterie) {
+		
+		this.idmaterie = idmaterie;//nel db potremmo chiamarlo idMateria
+		MateriaDAO materia = new MateriaDAO(idmaterie); //carico dal DB con la PK
+		this.nome = materia.getNome();
+		valutazioni = new ArrayList<EntityValutazione>();
+		
+		materia.caricaValutazioniDaDB();
+		caricaValutazione(materia);
+					
+	}
+	
 
+	//costruttore copia
+	public EntityMateria(MateriaDAO materia) {
+		
+		this.idmaterie = materia.getIdmateria();
+		this.nome = materia.getNome();
+		valutazioni = new ArrayList<EntityValutazione>();
+		
+		caricaValutazione(materia);
+		
+	}
+	
+	public void caricaValutazione(MateriaDAO materia) {
+		for(int i=0; i<materia.getValutazioni().size(); i++) {
+			
+			EntityValutazione valutazione = new EntityValutazione(materia.getValutazioni().get(i)); //va implementato il copy in Valutazioni
+			this.valutazioni.add(valutazione);
+		}
+	}
+	
 	public int getIdmaterie() {
 		return idmaterie;
 	}
@@ -43,6 +72,11 @@ public class EntityMateria {
 
 	public void setValutazioni(ArrayList<EntityValutazione> valutazioni) {
 		this.valutazioni = valutazioni;
+	}
+
+	@Override
+	public String toString() {
+		return "EntityMateria [idmaterie=" + idmaterie + ", nome=" + nome + ", valutazioni=" + valutazioni + "]";
 	}
 	
 	
