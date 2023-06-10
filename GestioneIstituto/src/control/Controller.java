641a58164f9e9d9762705262887b682e22bd26e0
Cris <111997390+iris-cmd22@users.noreply.github.com>
2023-06-10 16:04:53 +0200

    Update Controller.java

--- GestioneIstituto/src/control/Controller.java
+++ GestioneIstituto/src/control/Controller.java
@@ -1,138 +1,67 @@
 package control;
 
 import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.Year;
 import java.util.ArrayList;
 
-import database.DBConnectionManager;
-import entity.EntityValutazione;
+import entity.EntityIstituto;
 
 public class Controller {
 
-    public Controller(){
-
-    }
-
-    public static int registraUtente(){
-
-        int ret=0;
-
-        return ret;
-    }
-
-    /*public static int inserisciClasse(int idClasse, String sezione, String anno, int annoscolastico, ArrayList<int> idmaterie){
-
-        int ret=0;
-
-        return ret;
-    }*/
-
-    public static int aggiungiVoto(int idvalutazioni, int matricola, int idmaterie, Date data, int voto){
-    	EntityValutazione valutazione = new EntityValutazione();
+	public Controller() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+    public static int registraUtente(String ruolo,String nome,String cognome,Date dataNascita, String codiceFiscale, 
+    								String comuneResidenza, String email,int numeroCellulare, String username,
+    								String password,ArrayList<Integer> materie, String figlio, int classe ){
+    	int ret=0;
     	
-    	    	
-    	 // Controllo se esiste uno studente con quella matricola
-        if (!esisteStudente(matricola)) {
-            return -1;
-        }
-
-        // Controllo se esiste una materia con idmaterie
-        if (!esisteMateria(idmaterie)) {
-            return -1;
-        }
-
-        // Controllo se la data è nel quadrimestre corrente
-        if (!isDataValida(data)) {
-            return -1;
-        }
-
-        // Controllo se il voto è compreso tra 0 e 10
-        if (voto < 0 || voto > 10) {
-            return -1;
-        }
-
+    	EntityIstituto singleton = EntityIstituto.getInstance();
     	
-    	
-    	
-    	int ret = valutazione.scriviSuDB(idvalutazioni, matricola,idmaterie,data,voto);
-    	
-    	if(ret!=-1) {
-    		valutazione.setData(data);
-    		valutazione.setVoto(voto);
+    	if(ruolo=="docente") {
     		
+    		ret=singleton.creaDocente(nome,cognome,dataNascita, codiceFiscale, 
+					comuneResidenza,email,numeroCellulare,username,password,materie);
     		
+    	}else if(ruolo=="genitore") {
+    		
+    		ret=singleton.creaStudente(nome,cognome,dataNascita, codiceFiscale, 
+					comuneResidenza,email,numeroCellulare,username,password, classe);
+    		
+    	}else if(ruolo=="studente") {
+    		
+    		ret=singleton.creaGenitore(nome,cognome,dataNascita, codiceFiscale, 
+					comuneResidenza,email,numeroCellulare,username,password,figlio);
+    	}else {
+    		
+    		System.out.println("Ruolo non valido");
+    		ret=-1;
     	}
+    	
+        return ret;
+    }
 
-      
+
+    public static int aggiungiVoto(int matricola, int materia, Date data, float voto){
+    	
+    	
+    	EntityIstituto singleton = EntityIstituto.getInstance();
+        int ret=singleton.aggiungiVoto(matricola,materia,data,voto);
 
         return ret;
-    
     }
-    
-    
-    
-    public static boolean esisteStudente(int matricola) {
-        try {
-            ResultSet rs = DBConnectionManager.selectQuery("SELECT matricola FROM studenti WHERE matricola = '"+matricola+"';");;
-            return rs.next();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public static boolean esisteMateria(int idmaterie){
-        try {
-        	
-            ResultSet rs = DBConnectionManager.selectQuery("SELECT idmaterie FROM materie WHERE idmaterie = "+idmaterie+";");
-
-            return rs.next();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-
-      public static boolean isDataValida(Date data) { //in ingresso c'è la data della valutazione, che non è necessariamente la data corrente
-    	 LocalDate dataCorrente = LocalDate.now();
-    	    int annoCorrente = dataCorrente.getYear();
-    	    int meseCorrente = dataCorrente.getMonthValue();
-    	    int annoInput = data.toLocalDate().getYear();
-    	    int meseInput = data.toLocalDate().getMonthValue();
-    	    LocalDate convertedDate = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-            //Si controlla che la data della valutazione sia nel quadrimestre (e anno) corrente, e che non sia successiva alla data corrente
-          
-    	    // Primo quadrimestre: settembre - gennaio: 
-    	    if ((meseCorrente >= 9 || meseCorrente <= 1) && (annoCorrente  == annoInput || annoCorrente == annoInput +1) && dataCorrente.compareTo(convertedDate)<=0) {
-    	        if (meseInput >= 9 || meseInput <= 1) {
-    	            return true;
-    	        }
-    	    }
-    	    // Secondo quadrimestre: febbraio - giugno
-    	    else if (meseCorrente >= 2 && meseCorrente <= 6 && annoCorrente == annoInput && dataCorrente.compareTo(convertedDate)<=0) {
-    	        if (meseInput >= 2 && meseInput <= 6) {
-    	            return true;
-    	        }
-    	    }
-
-    	    return false;
-    }
-
-
-    
-    
     
     public static ArrayList<String> visualizzaVoti(String usernameGenitore){
 
+    	EntityIstituto singleton = EntityIstituto.getInstance();
         ArrayList<String> valutazioni = new ArrayList<String>();
+        
+        valutazioni= singleton.getListaValutazioni(usernameGenitore);
 
         return valutazioni;
     }
 
 
 }
+
