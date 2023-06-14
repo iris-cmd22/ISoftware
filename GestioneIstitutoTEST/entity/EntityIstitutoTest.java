a22d48ebe00d98103032de714bfb966fe9749448
afladrosa <128139011+afladrosa@users.noreply.github.com>
2023-06-14 12:32:10 +0200

    Update EntityIstitutoTest.java

--- GestioneIstitutoTEST/entity/EntityIstitutoTest.java
+++ GestioneIstitutoTEST/entity/EntityIstitutoTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.sql.Date;
+import java.time.LocalDate;
+
 
 public class EntityIstitutoTest {
 
@@ -22,12 +24,15 @@ public class EntityIstitutoTest {
 
 	@Test
 	public void testCreaDocente() { 
-			
+	
+		
 		EntityIstituto singleton = EntityIstituto.getInstance();
 	
 		ArrayList<Integer> materie= new ArrayList<Integer>();
 		materie.add(1);
-		Date datanascita = new Date(1975-11-30);
+		
+		LocalDate localDate = LocalDate.of(1975,11,30);
+		 java.sql.Date datanascita = java.sql.Date.valueOf(localDate);
 		int control=singleton.creaDocente("Luigi", "Bianchi", datanascita , "BNCLGU75S30H703D", "Salerno", "luigibianchi75@gmail.com", "+338763435", "l.bianchi30","BianchiPass3@" , materie);
 		System.out.println(control);
 		
@@ -35,12 +40,29 @@ public class EntityIstitutoTest {
 		
 		assertEquals(expected, control);		
 		
+		
 	}
 
 	
 	@Test
 	public void testCreaStudente() {
-		fail("Not yet implemented");
+		EntityIstituto singleton = EntityIstituto.getInstance();
+		
+		
+		
+		LocalDate localDate = LocalDate.of(2001,10,20);
+		 java.sql.Date datanascita = java.sql.Date.valueOf(localDate);
+		
+		int control=singleton.creaStudente("Mario", "Rossi", datanascita , "RSSMRA01R20F839P", "Napoli", "mariorossi2001@gmail.com", "338765432", "m.rossi20","ScuolaMario20@" , 1);
+		
+		
+	
+
+		System.out.println(control);
+		
+		int expected=1;
+		
+		assertEquals(expected, control);	
 	}
 
 	@Test
@@ -53,6 +75,7 @@ public class EntityIstitutoTest {
 		fail("Not yet implemented");
 	}
 
+
 	@Test
 	
 	 public void testAggiungiVoto() {
@@ -61,12 +84,12 @@ public class EntityIstitutoTest {
 	   // Preparazione dei dati di test
 	
 	 LocalDate localDate = LocalDate.of(2023, 06, 10);
-	 java.sql.Date sqlDate = java.sql.Date.valueOf(localDate);
+	 java.sql.Date datavalutazione = java.sql.Date.valueOf(localDate);
 
 	   float votoValido = (float)7.5;
 	   
 	   // Esecuzione del metodo con un voto valido
-	   int control = singleton.aggiungiVoto("l.bianchi30", 4, 1,sqlDate, votoValido);
+	   int control = singleton.aggiungiVoto("l.bianchi30", 1, 1,datavalutazione, votoValido);
 	   System.out.println(control);
 	   int expected=1;
 	   
@@ -76,7 +99,7 @@ public class EntityIstitutoTest {
 	   float votoMaggiore = 11;
 	   
 	   // Esecuzione del metodo con un voto valido
-	    control = singleton.aggiungiVoto("l.bianchi30", 4, 1,sqlDate, votoMaggiore);
+	    control = singleton.aggiungiVoto("l.bianchi30", 1, 1,datavalutazione, votoMaggiore);
 	   System.out.println(control);
 	   expected=-2;
 	   
@@ -87,7 +110,7 @@ public class EntityIstitutoTest {
 	   float votoMinore = (float) 0.5;
 	   
 	   // Esecuzione del metodo con un voto valido
-	    control = singleton.aggiungiVoto("l.bianchi30", 4, 1,sqlDate, votoMinore);
+	    control = singleton.aggiungiVoto("l.bianchi30", 1, 1,datavalutazione, votoMinore);
 	   System.out.println(control);
 	   expected=-2;
 	   
@@ -97,4 +120,4 @@ public class EntityIstitutoTest {
 	}
 	}
 
-}
+
